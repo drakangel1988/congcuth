b67c31c7192523425088efb27ea41895b63ab702
sinhvien <sinhvien>
2021-03-31 13:43:26 +0700

    second commit

---
+++ congcuform/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace congcuform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTaikhoan = new System.Windows.Forms.Label();
+            this.lbMatkhau = new System.Windows.Forms.Label();
+            this.txtTaikhoan = new System.Windows.Forms.TextBox();
+            this.txtMatkhau = new System.Windows.Forms.TextBox();
+            this.btnDangnhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTaikhoan
+            // 
+            this.lbTaikhoan.AutoSize = true;
+            this.lbTaikhoan.Location = new System.Drawing.Point(32, 78);
+            this.lbTaikhoan.Name = "lbTaikhoan";
+            this.lbTaikhoan.Size = new System.Drawing.Size(56, 13);
+            this.lbTaikhoan.TabIndex = 0;
+            this.lbTaikhoan.Text = "Tai Khoan";
+            // 
+            // lbMatkhau
+            // 
+            this.lbMatkhau.AutoSize = true;
+            this.lbMatkhau.Location = new System.Drawing.Point(32, 117);
+            this.lbMatkhau.Name = "lbMatkhau";
+            this.lbMatkhau.Size = new System.Drawing.Size(53, 13);
+            this.lbMatkhau.TabIndex = 1;
+            this.lbMatkhau.Text = "Mat Khau";
+            // 
+            // txtTaikhoan
+            // 
+            this.txtTaikhoan.Location = new System.Drawing.Point(106, 75);
+            this.txtTaikhoan.Name = "txtTaikhoan";
+            this.txtTaikhoan.Size = new System.Drawing.Size(100, 20);
+            this.txtTaikhoan.TabIndex = 2;
+            // 
+            // txtMatkhau
+            // 
+            this.txtMatkhau.Location = new System.Drawing.Point(106, 117);
+            this.txtMatkhau.Name = "txtMatkhau";
+            this.txtMatkhau.Size = new System.Drawing.Size(100, 20);
+            this.txtMatkhau.TabIndex = 3;
+            // 
+            // btnDangnhap
+            // 
+            this.btnDangnhap.Location = new System.Drawing.Point(56, 206);
+            this.btnDangnhap.Name = "btnDangnhap";
+            this.btnDangnhap.Size = new System.Drawing.Size(75, 23);
+            this.btnDangnhap.TabIndex = 4;
+            this.btnDangnhap.Text = "Dang Nhap";
+            this.btnDangnhap.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(217, 206);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 23);
+            this.btnThoat.TabIndex = 5;
+            this.btnThoat.Text = "Thoat";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 341);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangnhap);
+            this.Controls.Add(this.txtMatkhau);
+            this.Controls.Add(this.txtTaikhoan);
+            this.Controls.Add(this.lbMatkhau);
+            this.Controls.Add(this.lbTaikhoan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTaikhoan;
+        private System.Windows.Forms.Label lbMatkhau;
+        private System.Windows.Forms.TextBox txtTaikhoan;
+        private System.Windows.Forms.TextBox txtMatkhau;
+        private System.Windows.Forms.Button btnDangnhap;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
